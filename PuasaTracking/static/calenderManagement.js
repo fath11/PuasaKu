a82fd80c9e778d7aeb89efc3a3b0ee437922172a
fath11 <fathimkamal@gmail.com>
2024-02-15 17:16:04 +0800

    Implemented dynamic ramadan filtering with aladhan

--- PuasaTracking/static/calenderManagement.js
+++ PuasaTracking/static/calenderManagement.js
@@ -3,34 +3,46 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendar = new FullCalendar.Calendar(calendarEl, {
         height: 650,
         initialView: 'dayGridMonth',
-        events: [
-            ...getCurrentFastingProgress()
-        ],
         themeSystem: 'bootstrap',
+        datesSet: function() {
+            // Get the current view's date range
+            var start = calendar.view.currentStart;
+
+            // Fetch the events for the current view
+            getCurrentFastingProgress(start.getFullYear(), start.getMonth() + 1)
+                .then(events => {
+                    calendar.removeAllEventSources();
+                    calendar.addEventSource(events);
+                });
+        },
     });
     calendar.render();
 });
 
-function getCurrentFastingProgress() {
-    // Initialize the start date
-    let startDate = new Date('2024-03-11');
+async function getCurrentFastingProgress(year, month) {
+    console.log(`${month} and ${year}`)
+    let response = await fetch(`https://api.aladhan.com/v1/gToHCalendar/${month}/${year}`);
+    let data = await response.json();
+
+    // Filter the data to get the dates for Ramadan
+    let ramadanDates = data.data.filter(item => item.hijri.month.number == 9);
+    console.log(ramadanDates)
 
     // Initialize the events array
     let events = [];
 
-    // Loop 30 times
-    for (let i = 0; i < 30; i++) {
-        // Create a new date object for the start date plus i days
-        let eventDate = new Date(startDate.getTime());
-        eventDate.setDate(startDate.getDate() + i);
+    // Loop through each day of Ramadan
+    for (let i = 0; i < ramadanDates.length; i++) {
+        // Get the date for the current day of Ramadan
+        let ramadanToGregorian = ramadanDates[i].gregorian.date;
+        let parts = ramadanToGregorian.split('-');
+        let FullCalendarSupportableDate = new Date(`${parts[2]}-${parts[1]}-${parts[0]}`)
 
-        // Format the date as a string in the format 'yyyy-mm-dd'
-        let eventDateString = eventDate.toISOString().split('T')[0];
 
         // Create an event object
         let event = {
-            "title": 'Day' + (i + 1),
-            "start": eventDateString,
+            "title": 'Day ' + (i + 1),
+            "start": FullCalendarSupportableDate.toISOString().split("T")[0],
             "display": 'background',
             "color": 'red',
             "textColor": 'black',
